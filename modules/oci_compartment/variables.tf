4a8686888138945ebdabc8cbb868902241900681
Rahul <46425314+rahuldasdevops@users.noreply.github.com>
2026-01-07 18:59:48 +0530

    Update variables.tf

--- modules/oci_compartment/variables.tf
+++ modules/oci_compartment/variables.tf
@@ -1,12 +1,4 @@
-variable "compartment_id" {
-  type = string
-}
-variable "description" {
-  type = string
-}
-variable "name" {
-  type = string
-}
-variable "current_user_ocid" {
-  type = string
-}
+variable "compartment_id" {}
+variable "description" {}
+variable "name" {}
+variable "current_user_ocid" {}
